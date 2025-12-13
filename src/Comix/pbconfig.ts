778a2bd17d2f0cbbd08d164190d797156272c114
Kinso <kinso@example.com>
2025-12-13 12:42:55 +0000

    Bump version to force cache clear

--- src/Comix/pbconfig.ts
+++ src/Comix/pbconfig.ts
@@ -3,7 +3,7 @@ import { ContentRating, SourceIntents, type ExtensionInfo } from "@paperback/typ
 export default {
   name: "Comix",
   description: "Extension that pulls content from Comix.to",
-  version: "0.0.01",
+  version: "0.0.02",
   icon: "icon.png",
   language: "en",
   contentRating: ContentRating.EVERYONE,
