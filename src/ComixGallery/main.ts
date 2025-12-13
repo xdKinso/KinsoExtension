de0cfbe32e8491cdd27b2e1a13c80af43d38a56c
Kinso <kinso@example.com>
2025-12-13 13:43:01 +0000

    Fix getFilters call with required parameter

--- src/ComixGallery/main.ts
+++ src/ComixGallery/main.ts
@@ -1,22 +1,22 @@
 import {
-  DiscoverSectionType,
-  Form,
-  type Chapter,
-  type ChapterDetails,
-  type ChapterProviding,
-  type DiscoverSection,
-  type DiscoverSectionItem,
-  type DiscoverSectionProviding,
-  type Extension,
-  type MangaProviding,
-  type PagedResults,
-  type SearchFilter,
-  type SearchQuery,
-  type SearchResultItem,
-  type SearchResultsProviding,
-  type SettingsFormProviding,
-  type SortingOption,
-  type SourceManga,
+    DiscoverSectionType,
+    Form,
+    type Chapter,
+    type ChapterDetails,
+    type ChapterProviding,
+    type DiscoverSection,
+    type DiscoverSectionItem,
+    type DiscoverSectionProviding,
+    type Extension,
+    type MangaProviding,
+    type PagedResults,
+    type SearchFilter,
+    type SearchQuery,
+    type SearchResultItem,
+    type SearchResultsProviding,
+    type SettingsFormProviding,
+    type SortingOption,
+    type SourceManga,
 } from "@paperback/types";
 import { Forms } from "./forms";
 import type { Metadata } from "./models";
@@ -27,98 +27,107 @@ import { globalFilters } from "./utils";
 export const parse = new JsonParser();
 export const filter = new globalFilters();
 type ComixGalleryImplementation = SettingsFormProviding &
-  Extension &
-  DiscoverSectionProviding &
-  SearchResultsProviding &
-  MangaProviding &
-  ChapterProviding;
+    Extension &
+    DiscoverSectionProviding &
+    SearchResultsProviding &
+    MangaProviding &
+    ChapterProviding;
 
 export class ComixGalleryExtension implements ComixGalleryImplementation {
-  async getSettingsForm(): Promise<Form> {
-    return new Forms();
-  }
-
-  async initialise(): Promise<void> {
-    // Extension initialized
-  }
-
-  async getDiscoverSections(): Promise<DiscoverSection[]> {
-    const get_popular: DiscoverSection = {
-      id: "popular",
-      title: "Popular",
-      type: DiscoverSectionType.featured,
-    };
-    const get_follow: DiscoverSection = {
-      id: "follow",
-      title: "Most Follows New Comics",
-      type: DiscoverSectionType.prominentCarousel,
-    };
-    const get_recent: DiscoverSection = {
-      id: "recent",
-      title: "Recently Added",
-      type: DiscoverSectionType.simpleCarousel,
-    };
-
-    const get_updatesHot: DiscoverSection = {
-      id: "updatesHot",
-      title: "Latest Updates (HOT)",
-      type: DiscoverSectionType.chapterUpdates,
-    };
-
-    const get_updatesNew: DiscoverSection = {
-      id: "updatesNew",
-      title: "Latest Updates (NEW)",
-      type: DiscoverSectionType.chapterUpdates,
-    };
-
-    return [get_popular, get_follow, get_recent, get_updatesHot, get_updatesNew];
-  }
-
-  async getDiscoverSectionItems(
-    section: DiscoverSection,
-    metadata: Metadata,
-  ): Promise<PagedResults<DiscoverSectionItem>> {
-    switch (section.id) {
-      case "popular":
-        return await parse.parseSectionPopular("popular");
-      case "follow":
-        return await parse.parseSectionFollow("follow");
-      case "recent":
-        return await parse.parseSectionRecent("recent", metadata);
-      case "updatesNew":
-        return await parse.parseSectionChUp("updatesNew", metadata);
-      case "updatesHot":
-        return await parse.parseSectionChUp("updatesHot", metadata);
-      default:
-        return { items: [] };
+    async getSettingsForm(): Promise<Form> {
+        return new Forms();
     }
-  }
 
-  async getSearchFilters(): Promise<SearchFilter[]> {
-    return filter.getFilters(parse.parseFilterUpdate.bind(parse));
-  }
+    mainInterceptor = new MainInterceptor("main");
 
-  getSearchResults(
-    query: SearchQuery,
-    metadata: Metadata | undefined,
-    sortingOption: SortingOption,
-  ): Promise<PagedResults<SearchResultItem>> {
-    return parse.parseSearchResults(query, metadata, sortingOption);
-  }
-  async getSortingOptions(): Promise<SortingOption[]> {
-    return filter.order;
-  }
+    async initialise(): Promise<void> {
+        mainRateLimiter.registerInterceptor();
+        this.mainInterceptor.registerInterceptor();
+    }
 
-  getMangaDetails(mangaId: string): Promise<SourceManga> {
-    return parse.parseMangaDetails(mangaId);
-  }
+    async getDiscoverSections(): Promise<DiscoverSection[]> {
+        const get_popular: DiscoverSection = {
+            id: "popular",
+            title: "Popular",
+            type: DiscoverSectionType.featured,
+        };
+        const get_follow: DiscoverSection = {
+            id: "follow",
+            title: "Most Follows New Comics",
+            type: DiscoverSectionType.prominentCarousel,
+        };
+        const get_recent: DiscoverSection = {
+            id: "recent",
+            title: "Recently Added",
+            type: DiscoverSectionType.simpleCarousel,
+        };
 
-  getChapters(sourceManga: SourceManga): Promise<Chapter[]> {
-    return parse.parseChapters(sourceManga);
-  }
-  getChapterDetails(chapter: Chapter): Promise<ChapterDetails> {
-    return parse.parseChapterDetails(chapter.chapterId);
-  }
+        const get_updatesHot: DiscoverSection = {
+            id: "updatesHot",
+            title: "Latest Updates (HOT)",
+            type: DiscoverSectionType.chapterUpdates,
+        };
+
+        const get_updatesNew: DiscoverSection = {
+            id: "updatesNew",
+            title: "Latest Updates (NEW)",
+            type: DiscoverSectionType.chapterUpdates,
+        };
+
+        return [
+            get_popular,
+            get_follow,
+            get_recent,
+            get_updatesHot,
+            get_updatesNew,
+        ];
+    }
+
+    async getDiscoverSectionItems(
+        section: DiscoverSection,
+        metadata: Metadata,
+    ): Promise<PagedResults<DiscoverSectionItem>> {
+        switch (section.id) {
+            case "popular":
+                return await parse.parseSectionPopular("popular");
+            case "follow":
+                return await parse.parseSectionFollow("follow");
+            case "recent":
+                return await parse.parseSectionRecent("recent", metadata);
+            case "updatesNew":
+                return await parse.parseSectionChUp("updatesNew", metadata);
+            case "updatesHot":
+                return await parse.parseSectionChUp("updatesHot", metadata);
+            default:
+                return { items: [] };
+        }
+    }
+
+    async getSearchFilters(): Promise<SearchFilter[]> {
+        return filter.getFilters(parse.parseFilterUpdate.bind(parse));
+    }
+
+    getSearchResults(
+        query: SearchQuery,
+        metadata: Metadata | undefined,
+        sortingOption: SortingOption,
+    ): Promise<PagedResults<SearchResultItem>> {
+        return parse.parseSearchResults(query, metadata, sortingOption);
+    }
+    async getSortingOptions(): Promise<SortingOption[]> {
+        return filter.order;
+    }
+
+    getMangaDetails(mangaId: string): Promise<SourceManga> {
+        return parse.parseMangaDetails(mangaId);
+    }
+
+    getChapters(sourceManga: SourceManga): Promise<Chapter[]> {
+        return parse.parseChapters(sourceManga);
+    }
+    getChapterDetails(chapter: Chapter): Promise<ChapterDetails> {
+        return parse.parseChapterDetails(chapter.chapterId);
+    }
 }
 
-export default new ComixGalleryExtension();
+export const ComixGallery = new ComixGalleryExtension();
