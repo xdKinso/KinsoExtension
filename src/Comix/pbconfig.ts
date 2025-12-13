00905d740ac183d03fd05d3564b48aa8934a5211
Kinso <kinso@example.com>
2025-12-13 12:52:55 +0000

    version change

--- src/Comix/pbconfig.ts
+++ src/Comix/pbconfig.ts
@@ -3,7 +3,7 @@ import { ContentRating, SourceIntents, type ExtensionInfo } from "@paperback/typ
 export default {
   name: "Comix",
   description: "Extension that pulls content from Comix.to",
-  version: "0.0.02",
+  version: "0.0.03",
   icon: "icon.png",
   language: "en",
   contentRating: ContentRating.EVERYONE,
