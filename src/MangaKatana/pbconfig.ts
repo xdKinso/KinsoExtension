72fbd37e9cf2a1d8e8f2d87a028d534b2b69774b
Kinso <kinso@example.com>
2025-12-13 22:16:05 +0000

    Remove getImageRequest from MangaKatana to match working AsuraScans implementation

--- src/MangaKatana/pbconfig.ts
+++ src/MangaKatana/pbconfig.ts
@@ -7,7 +7,7 @@ import {
 export default {
     name: "MangaKatana",
     description: "Extension that pulls content from mangakatana.com.",
-    version: "0.0.13",
+    version: "0.0.15",
     icon: "icon.png",
     language: "en",
     contentRating: ContentRating.EVERYONE,
