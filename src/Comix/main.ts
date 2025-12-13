ea7b1ea63d5a3e5fd6204e331d41728267bbcb8d
Kinso <kinso@example.com>
2025-12-13 11:53:23 +0000

    Add complete working Comix extension with parsers and filters

--- src/Comix/main.ts
+++ src/Comix/main.ts
@@ -1,124 +1,133 @@
-﻿/* SPDX-License-Identifier: GPL-3.0-or-later */
-/* Copyright  2025 Kinso */
-
 import {
-  BasicRateLimiter,
-  ContentRating,
-  DiscoverSectionType,
-  Form,
-  type Chapter,
-  type ChapterDetails,
-  type ChapterProviding,
-  type DiscoverSection,
-  type DiscoverSectionItem,
-  type DiscoverSectionProviding,
-  type Extension,
-  type MangaProviding,
-  type PagedResults,
-  type SearchFilter,
-  type SearchQuery,
-  type SearchResultItem,
-  type SearchResultsProviding,
-  type SettingsFormProviding,
-  type SourceManga,
-  type Tag,
-} from '@paperback/types';
+    DiscoverSectionType,
+    Form,
+    type Chapter,
+    type ChapterDetails,
+    type ChapterProviding,
+    type DiscoverSection,
+    type DiscoverSectionItem,
+    type DiscoverSectionProviding,
+    type Extension,
+    type MangaProviding,
+    type PagedResults,
+    type SearchFilter,
+    type SearchQuery,
+    type SearchResultItem,
+    type SearchResultsProviding,
+    type SettingsFormProviding,
+    type SortingOption,
+    type SourceManga,
+} from "@paperback/types";
+import { Forms } from "./forms";
+import type { Metadata } from "./models";
+import { MainInterceptor, mainRateLimiter } from "./network";
+import { JsonParser } from "./parsers";
+import { globalFilters } from "./utils";
 
-import { SettingsForm } from './forms';
-import { MainInterceptor } from './network';
+export const parse = new JsonParser();
+export const filter = new globalFilters();
+type ComixToImplementation = SettingsFormProviding &
+    Extension &
+    DiscoverSectionProviding &
+    SearchResultsProviding &
+    MangaProviding &
+    ChapterProviding;
 
-const COMIX_DOMAIN = 'https://comix.to';
+export class ComiToExtension implements ComixToImplementation {
+    async getSettingsForm(): Promise<Form> {
+        return new Forms();
+    }
 
-type ComixImplementation = SettingsFormProviding &
-  Extension &
-  DiscoverSectionProviding &
-  SearchResultsProviding &
-  MangaProviding &
-  ChapterProviding;
+    mainInterceptor = new MainInterceptor("main");
 
-export class ComixExtension implements ComixImplementation {
-  mainRateLimiter = new BasicRateLimiter('main', {
-    numberOfRequests: 4,
-    bufferInterval: 1,
-    ignoreImages: true,
-  });
+    async initialise(): Promise<void> {
+        mainRateLimiter.registerInterceptor();
+        this.mainInterceptor.registerInterceptor();
+    }
 
-  mainInterceptor = new MainInterceptor('main');
+    async getDiscoverSections(): Promise<DiscoverSection[]> {
+        const get_popular: DiscoverSection = {
+            id: "popular",
+            title: "Popular",
+            type: DiscoverSectionType.featured,
+        };
+        const get_follow: DiscoverSection = {
+            id: "follow",
+            title: "Most Follows New Comics",
+            type: DiscoverSectionType.prominentCarousel,
+        };
+        const get_recent: DiscoverSection = {
+            id: "recent",
+            title: "Recently Added",
+            type: DiscoverSectionType.simpleCarousel,
+        };
 
-  async initialise(): Promise<void> {
-    this.mainRateLimiter.registerInterceptor();
-    this.mainInterceptor.registerInterceptor();
-  }
+        const get_updatesHot: DiscoverSection = {
+            id: "updatesHot",
+            title: "Latest Updates (HOT)",
+            type: DiscoverSectionType.chapterUpdates,
+        };
 
-  async getSettingsForm(): Promise<Form> {
-    return new SettingsForm();
-  }
+        const get_updatesNew: DiscoverSection = {
+            id: "updatesNew",
+            title: "Latest Updates (NEW)",
+            type: DiscoverSectionType.chapterUpdates,
+        };
 
-  async getDiscoverSections(): Promise<DiscoverSection[]> {
-    return [
-      {
-        id: 'latest-updates',
-        title: 'Latest Updates',
-        type: DiscoverSectionType.simpleCarousel,
-      },
-      {
-        id: 'popular',
-        title: 'Popular',
-        type: DiscoverSectionType.simpleCarousel,
-      },
-    ];
-  }
+        return [
+            get_popular,
+            get_follow,
+            get_recent,
+            get_updatesHot,
+            get_updatesNew,
+        ];
+    }
 
-  async getDiscoverSectionItems(
-    section: DiscoverSection,
-    metadata: unknown,
-  ): Promise<PagedResults<DiscoverSectionItem>> {
-    const items: DiscoverSectionItem[] = [];
-    return {
-      items,
-      metadata,
-    };
-  }
+    async getDiscoverSectionItems(
+        section: DiscoverSection,
+        metadata: Metadata,
+    ): Promise<PagedResults<DiscoverSectionItem>> {
+        switch (section.id) {
+            case "popular":
+                return await parse.parseSectionPopular("popular");
+            case "follow":
+                return await parse.parseSectionFollow("follow");
+            case "recent":
+                return await parse.parseSectionRecent("recent", metadata);
+            case "updatesNew":
+                return await parse.parseSectionChUp("updatesNew", metadata);
+            case "updatesHot":
+                return await parse.parseSectionChUp("updatesHot", metadata);
+            default:
+                return { items: [] };
+        }
+    }
 
-  async getSearchFilters(): Promise<SearchFilter[]> {
-    return [];
-  }
+    async getSearchFilters(): Promise<SearchFilter[]> {
+        return filter.getFilters();
+    }
 
-  async getSearchResults(
-    query: SearchQuery,
-    metadata: unknown,
-  ): Promise<PagedResults<SearchResultItem>> {
-    return {
-      items: [],
-      metadata,
-    };
-  }
+    getSearchResults(
+        query: SearchQuery,
+        metadata: Metadata | undefined,
+        sortingOption: SortingOption,
+    ): Promise<PagedResults<SearchResultItem>> {
+        return parse.parseSearchResults(query, metadata, sortingOption);
+    }
+    async getSortingOptions(): Promise<SortingOption[]> {
+        return filter.order;
+    }
 
-  async getMangaDetails(mangaId: string): Promise<SourceManga> {
-    return {
-      mangaId,
-      mangaInfo: {
-        thumbnailUrl: '',
-        synopsis: 'Loading...',
-        primaryTitle: 'Test Manga',
-        secondaryTitles: [],
-        contentRating: ContentRating.EVERYONE,
-        status: 'Unknown',
-        author: '',
-        artist: '',
-      },
-    };
-  }
+    getMangaDetails(mangaId: string): Promise<SourceManga> {
+        return parse.parseMangaDetails(mangaId);
+    }
 
-  async getChapters(sourceManga: SourceManga): Promise<Chapter[]> {
-    return [];
-  }
-
-  async getChapterDetails(chapter: Chapter): Promise<ChapterDetails> {
-    return {
-      id: chapter.chapterId,
-      mangaId: chapter.sourceManga.mangaId,
-      pages: [],
-    };
-  }
+    getChapters(sourceManga: SourceManga): Promise<Chapter[]> {
+        return parse.parseChapters(sourceManga);
+    }
+    getChapterDetails(chapter: Chapter): Promise<ChapterDetails> {
+        return parse.parseChapterDetails(chapter.chapterId);
+    }
 }
+
+export const ComixTo = new ComiToExtension();
--- src/Comix/pbconfig.ts
+++ src/Comix/pbconfig.ts
@@ -1,25 +1,26 @@
-/* SPDX-License-Identifier: GPL-3.0-or-later */
-/* Copyright © 2025 Inkdex */
-
-import { ContentRating, SourceIntents, type ExtensionInfo } from "@paperback/types";
+import {
+    ContentRating,
+    SourceIntents,
+    type ExtensionInfo,
+} from "@paperback/types";
 
 export default {
-  name: "Comix",
-  description: "Extension for Comix - A manga/comic reading website at comix.to",
-  version: "1.0.0",
-  icon: "icon.png",
-  language: "en",
-  contentRating: ContentRating.MATURE,
-  capabilities:
-    SourceIntents.SETTINGS_FORM_PROVIDING |
-    SourceIntents.DISCOVER_SECIONS_PROVIDING |
-    SourceIntents.SEARCH_RESULTS_PROVIDING |
-    SourceIntents.CHAPTER_PROVIDING,
-  badges: [],
-  developers: [
-    {
-      name: "Kinso",
-      github: "https://github.com/xdKinso",
-    },
-  ],
+    name: "ComixTo",
+    description: "Extension that pulls content from Comix.to",
+    version: "1.0.0-alpha.4",
+    icon: "icon.png",
+    language: "en",
+    contentRating: ContentRating.EVERYONE,
+    capabilities:
+        SourceIntents.SETTINGS_FORM_PROVIDING |
+        SourceIntents.DISCOVER_SECIONS_PROVIDING |
+        SourceIntents.SEARCH_RESULTS_PROVIDING |
+        SourceIntents.CHAPTER_PROVIDING,
+    badges: [],
+    developers: [
+        {
+            name: "Catta1997",
+            github: "https://github.com/Catta1997",
+        },
+    ],
 } satisfies ExtensionInfo;
