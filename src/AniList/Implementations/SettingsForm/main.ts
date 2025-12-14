eb4064942e8ad5983e4010f5fd76f6d003442b25
Kinso <kinso@example.com>
2025-12-14 00:34:24 +0000

    Clean main branch - remove AniList and manual versioning, use auto-generated versioning from pbconfig

--- src/AniList/Implementations/SettingsForm/main.ts
+++
@@ -1,9 +0,0 @@
-import { Form } from "@paperback/types";
-import type { SettingsFormProviding } from "@paperback/types";
-import { SettingsForm } from "./form";
-
-export class SettingsFormImplementation implements SettingsFormProviding {
-  async getSettingsForm(): Promise<Form> {
-    return new SettingsForm();
-  }
-}
