7a091959da6e192129ce8ba99effbfbefe3cebad
kinso <mowinskikrystian@gmail.com>
2025-12-15 03:12:11 +0000

    ComixGallery v0.0.10: Remove volume field to fix chapter ordering issues

--- src/ComixGallery/parsers.ts
+++ src/ComixGallery/parsers.ts
@@ -144,10 +144,7 @@ export class JsonParser {
     allPages.sort((a, b) => a.page - b.page);
     const chaptersArray = allPages.flatMap((p) => p.data);
     return chaptersArray.map((chapter) => {
-      let version = chapter.scanlation_group?.name ?? "";
-      if (chapter.volume > 0) {
-        version = version.length > 0 ? `${version} Volumes` : "Volumes";
-      }
+      const version = chapter.scanlation_group?.name ?? "";
       return {
         chapterId: chapter.chapter_id.toString(),
         sourceManga: manga,
@@ -155,7 +152,6 @@ export class JsonParser {
         chapNum: chapter.number,
         title: chapter.name,
         version: version,
-        volume: chapter.volume,
         publishDate: new Date(chapter.updated_at * 1000),
         creationDate: new Date(chapter.created_at * 1000),
       };
