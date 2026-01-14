6d96b2d6e42d577158d071b0f2bb171f96f7004e
xdKinso <mowinskikrystian@gmail.com>
2026-01-14 14:33:02 +0000

    versioning

--- src/VyManga/pbconfig.ts
+++ src/VyManga/pbconfig.ts
@@ -3,7 +3,7 @@ import { ContentRating, SourceIntents, type SourceInfo } from "@paperback/types"
 export default {
   name: "VyManga",
   description: "Extension that pulls content from vymanga.com.",
-  version: "0.0.10",
+  version: "0.0.13",
   icon: "icon.png",
   language: "en",
   contentRating: ContentRating.MATURE,
